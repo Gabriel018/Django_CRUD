b42c5ddf9ae754ec9e647598edd39905796c72ee
Gabriel018 <duke.comandos@gmail.com>
2022-04-19 22:00:33 -0300

    1.1

--- Agenda/Contatos/views.py
+++ Agenda/Contatos/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Contatos
 # Create your views here.
 
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render({},request))
+
+def clientes(request):
+    template = loader.get_template('clientes.html')
+    client = Contatos.objects.all().values()
+
+    context = {
+        'client':client
+    }
+
+    return HttpResponse(template.render(context,request))
+
